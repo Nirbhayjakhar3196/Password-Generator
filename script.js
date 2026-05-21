19c76e90016c17ffbb8a717a86b316b3f067c516
nirbhayjakhar3196 <jakharnirbhay0000@gmail.com>
2026-05-21 16:26:49 +0530

    feat: Added password strong messsage indicator

--- script.js
+++ script.js
@@ -18,21 +18,28 @@ const copy = document.getElementById('copybtn')
 const lengthValue = document.getElementById('lengthValue')
 const minusbtn = document.getElementById('minusbtn')
 const plusbtn = document.getElementById('plusbtn')
+const strength = document.getElementById('strength')
 
 function generatePassword(){
     let allowedChars = ""
 
+    let strengthCheck = 0;
+
     if(uppercase.checked){
         allowedChars += upper;
+        strengthCheck += 1
     }
     if(lowercase.checked){
         allowedChars += lower;
+        strengthCheck += 1
     }
     if(numbersCheckbox.checked){
         allowedChars += numbers;
+        strengthCheck += 1
     }
     if(symbolsCheckbox.checked){
         allowedChars += symbols;
+        strengthCheck += 1
     } 
 
     let generatePassword = ""
@@ -50,6 +57,29 @@ function generatePassword(){
 
 
     passwordDisplay.value = generatePassword
+
+    if(passwordLength.value >= 12){
+        strengthCheck += 1
+    }
+
+        if(strengthCheck <= 2){
+        strength.textContent = "Weak"
+    }
+
+    else if(strengthCheck === 3){
+        strength.textContent = "Good"
+    }
+
+    else if(strengthCheck === 4){
+        strength.textContent = "Strong"
+    }
+
+    else{
+        strength.textContent = "Very Strong"
+    }
+    if(passwordLength.value <= 4){
+    strength.textContent = "Very Weak"
+}
 }
 
 
@@ -89,7 +119,7 @@ plusbtn.addEventListener('click',()=> {
 })
 minusbtn.addEventListener('click',()=> {
 
-    if(passwordLength.value >4){
+    if(passwordLength.value >1){
         passwordLength.value = Number(passwordLength.value) - 1
 
         lengthValue.textContent = passwordLength.value
